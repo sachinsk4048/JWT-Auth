97b191af3f239375c6d785d9901a9a84418c51c5
sachin <sachinsk30158@gmail.com>
2026-01-20 11:12:29 +0530

    auth complete

--- middlewares/authMiddleware.js
+++ middlewares/authMiddleware.js
@@ -5,7 +5,6 @@ const jwt = require('jsonwebtoken');
 
 exports.authMiddleware = async (req, res, next) => {
     try {
-        // const token = req.cookies.token || req.headers["authorization"]?.split(" ")[1];
         const token = req.cookies.token || req.headers["authorization"]?.split(" ")[1];
         if (!token) {
             return res.status(401).send({ message: 'unauthorize !token' });
